5fffa3d5642a6db31518051e87988c056abfaf5e
ImChrisH <hui.simplicity@gmail.com>
2024-07-29 23:09:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,22 +10,29 @@ totalBtn.addEventListener('click', tipCalc);
 
 function tipCalc(){
 let amount= parseFloat(tInput.value);
+
+//positive number check
 if (amount < 0){
         iTip.textContent=`Enter positive number`;
         totalOutput.textContent= `Enter positive number`;
         bill.textContent= `Enter positive number`;
+        return;
     }
-    
-else {
-let tip = amount >= 300 ? amount*.20: amount*.15;
+
+//ternary, calculation
+let tip = amount >= 300 ? amount*.20 :
+        amount >= 30 ? amount*.15 :
+        amount*.10;
 
 let total= amount+tip;
 
 bill.textContent=`$${amount.toFixed(2)}`;
 iTip.textContent=`$${tip.toFixed(2)}`;
 totalOutput.textContent= `$${total.toFixed(2)}`;
+
 }
-}
+
+//modal buttons
 
 let mBtn1 = document.querySelector('.mBtn1')  
 let mBtn2 = document.querySelector('.mBtn2')  
@@ -47,7 +54,7 @@ mBtn1.addEventListener('click', openModal(modal1));
 mBtn2.addEventListener('click', openModal(modal2));
 mBtn3.addEventListener('click', openModal(modal3));
 
-// test
+// testing closing windows
 // window.addEventListener('click', function (otherclose){
 //     if (otherclose=target===totalmodal) {
 //         modal1.style.display='none';
@@ -63,6 +70,5 @@ mBtn3.addEventListener('click', openModal(modal3));
 document.querySelectorAll('.close').forEach(closeBtn => {
     closeBtn.addEventListener('click', function() {
         this.closest('.modal1, .modal2, .modal3').style.display = 'none';
-        totalmodal.style.display = 'none';
-    });
-});
+        totalmodal.style.display = 'none';})
+    })
